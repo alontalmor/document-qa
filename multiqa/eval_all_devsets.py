597d409eccf4faf9ca40fa1c40dbbdf6e82c285b
Alon Talmor <salont@gmail.com>
2018-09-28 11:49:17 +0300

    training logs

--- multiqa/eval_all_devsets.py
+++ multiqa/eval_all_devsets.py
@@ -23,12 +23,12 @@ models_dir = join(CORPUS_DIR, "triviaqa", "web-open/")
 # running the docqa evaluation
 for dataset in args.datasets.split(','):
     if args.model == 'ALL':
-        for dirname, dirnames, filenames in os.walk(models_dir):
+        for dirname, dirnames, filenames in os.walk('models/'):
             for filename in filenames:
                 print('running triviaqa_full_document_eval')
                 print(models_dir + dataset)
                 print(models_dir + filename)
-                command = 'python docqa/eval/triviaqa_full_document_eval.py --n_processes 8 -c open-dev --tokens 800 -o question-output.json -p paragraph-output.csv ' + models_dir + filename + ' --source_dir ' + models_dir + dataset
+                command = 'python docqa/eval/triviaqa_full_document_eval.py --n_processes 8 -c open-dev --tokens 800 -o question-output.json -p paragraph-output.csv ' + 'models/' + filename + ' --source_dir ' + models_dir + dataset
                 print(command)
                 call(command, shell=True, preexec_fn=os.setsid)
     else:
